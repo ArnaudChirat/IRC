db2b4fb8993d57ccdb3b502c91a74f69bea02ffd
ArnaudChirat <arnaud.chirat@gmail.com>
2020-04-27 18:48:34 +0200

    Add length to servername

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 static const std::string word("[\\w]+");
 static const std::string shortname(word + "(?:-+" + word +")*");
 static const std::string hostname(shortname + R"((?:\.)" + shortname + ")*");
-static const std::string servername("^(?=.{1,63}$)" + hostname);
+static const std::string servername("^(?=.{1,63}\\w$)" + hostname);
 int         main() {
 
     try
